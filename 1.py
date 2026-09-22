349fca3fc3f48b473abce461be3ee343c992ed04
polatcaltntas <polatcanaltntas4@gmail.com>
2026-09-22 23:39:15 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,7 @@
 print("Boy gir")
 height=int(input())
+if height > 3:
+    height = height / 100
 print("Kilo gir")
 weight=int(input())
 
@@ -7,16 +9,15 @@ bki = weight / (height * height)
 
 if 25 < bki < 30:
     print("Aşırı kilolu")
-    weightchange = ((bki * (height * height)) - (25 * (height * height)))
+    weightchange = weight - (25 * (height * height))
     print("vermeniz gereken kilo",weightchange)
 elif 18.5 < bki < 25:
     print("sağlıklı kilolu")
 elif bki < 18.5:
     print("aşırı zayıf")
-    weightchange = (25 * height * height) -  (bki * height * height) 
+    weightchange = weight -  (18.5 * height * height) 
     print("almanız gereken kilo",weightchange)
 else:
     print("obez")
-    weightchange = (bki * height * height) - (25 * height * height)
+    weightchange = weight - (25 * height * height)
     print("vermeniz gereken kilo",weightchange)
-
